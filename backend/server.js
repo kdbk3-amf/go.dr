218a62cea3331529b27288975020ac18713b0b2e
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 23:24:14 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const authRoutes = require("./routes/auth.routes");
 const doctorRoutes = require("./routes/doctor.routes");
 const specialtyRoutes = require("./routes/specialty.routes");
 const hospitalRoutes = require("./routes/hospital.routes");
+const chamberRoutes=require("./routes/chamber.routes");
 const app = express();
 
 app.use(cors());
@@ -14,6 +15,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/doctors", doctorRoutes);
 app.use("/api/specialties", specialtyRoutes);
 app.use("/api/hospitals", hospitalRoutes);
+app.use("/api/chambers",chamberRoutes);
 app.get("/", (req, res) => {
   res.json({
     success: true,
