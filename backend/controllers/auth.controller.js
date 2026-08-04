67556d3959dcf790906d74617c300e220c2202a6
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 18:38:27 +0600

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,1 +1,13 @@
+exports.register = (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: "User registered successfully"
+  });
+};
 
+exports.login = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "User logged in successfully"
+  });
+};
