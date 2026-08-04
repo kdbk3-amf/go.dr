d83068675f980856d5163e793bcfbc9b9807481d
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 18:53:18 +0600

    Update doctor.controller.js

--- backend/controllers/doctor.controller.js
+++ backend/controllers/doctor.controller.js
@@ -1,1 +1,34 @@
+exports.getAllDoctors = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "All doctors fetched successfully"
+  });
+};
 
+exports.getDoctorById = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Doctor details fetched successfully"
+  });
+};
+
+exports.createDoctor = async (req, res) => {
+  res.status(201).json({
+    success: true,
+    message: "Doctor profile created successfully"
+  });
+};
+
+exports.updateDoctor = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Doctor profile updated successfully"
+  });
+};
+
+exports.deleteDoctor = async (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Doctor profile deleted successfully"
+  });
+};
