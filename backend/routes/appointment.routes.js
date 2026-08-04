03b78410aa8f553ba1cd5cf24cb3c2ef046437c6
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 23:28:43 +0600

    Update appointment.routes.js

--- backend/routes/appointment.routes.js
+++ backend/routes/appointment.routes.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
 
+const appointmentController = require("../controllers/appointment.controller");
+
+router.get("/", appointmentController.getAllAppointments);
+
+router.get("/:id", appointmentController.getAppointmentById);
+
+router.post("/", appointmentController.createAppointment);
+
+module.exports = router;
