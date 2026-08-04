622acff5bb7598e71df0abced8f22733742df2bf
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 19:22:25 +0600

    Update hospital.routes.js

--- backend/routes/hospital.routes.js
+++ backend/routes/hospital.routes.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const router = express.Router();
 
+const hospitalController = require("../controllers/hospital.controller");
+
+// Get all hospitals
+router.get("/", hospitalController.getAllHospitals);
+
+// Get hospital by ID
+router.get("/:id", hospitalController.getHospitalById);
+
+module.exports = router;
