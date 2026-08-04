e7cd6cba4b7570cf7a6b01bb31ef4f698994e91b
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 18:57:11 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
-
+const doctorRoutes = require("./routes/doctor.routes");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
-
+app.use("/api/doctors", doctorRoutes);
 app.get("/", (req, res) => {
   res.json({
     success: true,
