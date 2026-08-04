c2cf4fe39a45cc2e58f062f9985a31e19030bf8c
kdbk3-amf <kdbk2153@gmail.com>
2026-08-04 19:00:50 +0600

    Update doctor.model.js

--- backend/models/doctor.model.js
+++ backend/models/doctor.model.js
@@ -1,1 +1,18 @@
+const pool = require("../config/database");
 
+const Doctor = {
+  async getAll() {
+    const result = await pool.query("SELECT * FROM doctors");
+    return result.rows;
+  },
+
+  async getById(id) {
+    const result = await pool.query(
+      "SELECT * FROM doctors WHERE id = $1",
+      [id]
+    );
+    return result.rows[0];
+  }
+};
+
+module.exports = Doctor;
